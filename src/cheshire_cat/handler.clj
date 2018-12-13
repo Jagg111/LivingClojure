e45d695aa3336fb2d3ba3f575334f6227d977248
Mitchel Burton <jaggdev@gmail.com>
2018-12-13 11:30:52 -0600

    JSON conversion
    Added a new library.  Moved the data for the cat response to a json file.

--- src/cheshire_cat/handler.clj
+++ src/cheshire_cat/handler.clj
@@ -1,14 +1,24 @@
 (ns cheshire-cat.handler
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
+            ;; rings for web server thingz
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
             [ring.middleware.json :as ring-json]
-            [ring.util.response :as rr]))
+            [ring.util.response :as rr]
+            ;; json for messing with my json blob file
+            [clojure.data.json :as cjson]))
 
 (defroutes app-routes
   (GET "/" [] "Hello World")
   (GET "/cheshire-cat" []
-    (rr/response {:name "Cheshire Cat" :status :grinning}))
+    ;; Original line that works, body is a map
+    ;;(rr/response {:name "Cheshire Cat" :status :grinning}))
+
+    ;; This works but downloads a file likely because the body is a string
+    ;;(rr/response (slurp "resources/public/cats.json")))
+
+    ;; This works but I want to get the key as a proper key instead of a string
+    (rr/response (cjson/read-str (slurp "resources/public/cats.json"))))
   (route/not-found "Not Found"))
 
 (def app
